63668e1592c646c30269c552f962e5be0700427f
agros55 <fernandoviveros95@gmail.com>
2023-11-17 18:11:18 -0300

    Formulario

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,33 +1,58 @@
 import { Box } from '@chakra-ui/layout';
 import { Center, Button, FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
 import { Formik } from 'formik';
+import { useState } from 'react';
 import * as Yup from 'yup';
 
-const validaciones = Yup.object().shape({
-    nombre: Yup.string()
-        .min(5, 'Muy corto')
-        .max(50, 'Muy largo')
-        .required('El nombre es requerido.'),
-    apellido: Yup.string()
-        .min(5, 'Muy corto')
-        .max(50, 'Muy largo')
-        .required('El apellido es requerido.'),
-    password: Yup.string()
-        .min(6, 'Muy corto')
-        .max(50, 'Muy largo')
-        .required('El password es requerido.'),
-    direccion: Yup.string()
-        .min(5, 'Muy corto')
-        .max(50, 'Muy largo')
-        .required('La dirección es requerida.'),
-    email: Yup.string().email('Email inválido.').required('El email es requerido.'),
-});
 
 
 const FormOne = () => {
+    const validaciones = Yup.object().shape({
+        nombre: Yup.string()
+            .min(5, 'Debe contener al menos 5 caracteres.')
+            .max(50, 'Muy largo')
+            .required('El nombre es requerido.'),
+        apellido: Yup.string()
+            .min(5, 'Debe contener al menos 5 caracteres.')
+            .max(50, 'Muy largo')
+            .required('El apellido es requerido.'),
+        email: Yup.string()
+            .email('Email invalido')
+            .matches(/^[a-zA-z][a-zA-Z0-9-\.]{1,64}@[a-zA-Z0-9-]{1,64}((\.[a-zA-Z]{2,3})|(\.[a-zA-Z]{2,3}\.[a-zA-Z]{2,3}))$/, 'Email invalido')
+            .required('El email es requerido.'),
+        telefono: Yup.string()
+            .matches(/^[0-9]{10}$/, 'El numero debe contener 10 digitos.')
+            .required('El telefono es requerido.'),
+        password: Yup.string()
+            .min(6, 'Debe contener al menos 6 caracteres.')
+            .max(50, 'Muy largo')
+            .matches(/[0-9]/, '-Un numero.')
+            .matches(/[a-z]/, '-Una minuscula.')
+            .matches(/[A-Z]/, '-Una Mayuscula.')
+            .required('El password es requerido.'),
+        confirmarPassword: Yup.string()
+            .test('password-should-match', 'Las contraseñas no coinciden.', function (value) {
+                return this.parent.password === value
+            }),
+    });
+
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
+    const [mostrarPassword, setMostrarPassword] = useState(false)
+
+    function alternarPassword() {
+        setMostrarPassword(!mostrarPassword)
+    }
+
     return (
         <div>
             <Formik
+                onSubmit={(values) => {
+                    console.log(values)
+                    setIsSubmitting(true);
+                    console.log(isSubmitting)
+                }}
+                validationSchema={validaciones}
                 initialValues={{
                     nombre: '',
                     apellido: '',
@@ -35,34 +60,6 @@ const FormOne = () => {
                     telefono: '',
                     password: '',
                     confirmarPassword: '',
-                }}
-                validationSchema={validaciones}
-                /* validate={values => {
-                    const errors = {};
-                    if (!values.nombre) {
-                        errors.nombre = 'El nombre es requerido.';
-                    }
-                    if (!values.email) {
-                        errors.email = 'El email es requerido.';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Email invalido';
-                    }
-                    if (!values.password) {
-                        errors.password = 'El password es requerido';
-                    }
-                    if (!values.direccion) {
-                        errors.direccion = 'La dirección es requerida.';
-                    }
-
-                    return errors;
-                }} */
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
                 }}
             >
                 {({
@@ -76,7 +73,7 @@ const FormOne = () => {
                 }) => (
                     <Box w="50vw" p="50px">
                         <FormControl >
-                            <form onSubmit={handleSubmit}>
+                            <form disabled={"disabled"} onSubmit={handleSubmit}>
                                 {/* Campo para nombre */}
                                 <Box display="flex" justifyContent="space-between">
                                     <FormLabel >Nombre</FormLabel>
@@ -131,7 +128,7 @@ const FormOne = () => {
                                         value={values.telefono}
                                     />
                                 </Box>
-                                <Text textAlign="right">{errors.apellido && touched.apellido && errors.apellido}</Text>
+                                <Text textAlign="right">{errors.telefono && touched.telefono && errors.telefono}</Text>
 
                                 {/* Campo para password */}
                                 <Box display="flex" justifyContent="space-between">
@@ -139,12 +136,12 @@ const FormOne = () => {
                                     <Input w="80%"
                                         id='password'
                                         name='password'
-                                        type="password"
+                                        type={mostrarPassword ? 'text' : 'password'}
                                         onChange={handleChange}
                                         onBlur={handleBlur}
                                         value={values.password}
                                     />
-                                    <Button pos="absolute" right={0} zIndex={10}>Ver password</Button>
+                                    <Button onClick={alternarPassword} pos="absolute" right={0} zIndex={10}>Ver password</Button>
                                 </Box>
                                 <Text textAlign="right">{errors.password && touched.password && errors.password}</Text>
 
@@ -152,18 +149,18 @@ const FormOne = () => {
                                 <Box display="flex" justifyContent="space-between">
                                     <FormLabel>Confirmar password</FormLabel>
                                     <Input w="80%"
-                                        id='direccion'
-                                        name='direccion'
-                                        type='text'
+                                        id='confirmarPassword'
+                                        name='confirmarPassword'
+                                        type='password'
                                         onChange={handleChange}
                                         onBlur={handleBlur}
-                                        value={values.direccion}
+                                        value={values.confirmarPassword}
                                     />
                                 </Box>
-                                <Text textAlign="right">{errors.direccion && touched.direccion && errors.direccion}</Text>
+                                <Text textAlign="right">{errors.confirmarPassword && touched.confirmarPassword && errors.confirmarPassword}</Text>
                                 <br />
 
-                                <Button type='submit' disabled={isSubmitting}>Enviar</Button>
+                                <Button type='submit'>Enviar</Button>
 
                             </form>
                         </FormControl>
