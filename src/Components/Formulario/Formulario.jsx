e6cf60872c2c7773959c8740389e2573b37a1db6
agros55 <fernandoviveros95@gmail.com>
2023-11-17 17:54:15 -0300

    Configurando formulario

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -1,22 +1,62 @@
+import { Box } from '@chakra-ui/layout';
+import { Center, Button, FormControl, FormLabel, Input, Text } from '@chakra-ui/react';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const validaciones = Yup.object().shape({
+    nombre: Yup.string()
+        .min(2, 'Muy corto')
+        .max(50, 'Muy largo')
+        .required('El nombre es requerido.'),
+    password: Yup.string()
+        .min(2, 'Muy corto')
+        .max(50, 'Muy largo')
+        .required('El password es requerido.'),
+    direccion: Yup.string()
+        .min(2, 'Muy corto')
+        .max(50, 'Muy largo')
+        .required('La dirección es requerida.'),
+    email: Yup.string().email('Email inválido.').required('El email es requerido.'),
+});
+
 
 const FormOne = () => {
     return (
         <div>
             <Formik
                 initialValues={{
-                    name: '',
+                    nombre: '',
+                    email: '',
+                    password: '',
+                    direccion: '',
                 }}
-                onSubmit={(values) => {
-                    console.log(values)
-                    console.log(errors)
-                }}
-                validate={(values) => {
-                    const errors = {}
-                    if (values.name == '') {
-                        errors.name = 'Required'
+                validationSchema={validaciones}
+                /* validate={values => {
+                    const errors = {};
+                    if (!values.nombre) {
+                        errors.nombre = 'El nombre es requerido.';
                     }
-                    return errors
+                    if (!values.email) {
+                        errors.email = 'El email es requerido.';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = 'Email invalido';
+                    }
+                    if (!values.password) {
+                        errors.password = 'El password es requerido';
+                    }
+                    if (!values.direccion) {
+                        errors.direccion = 'La dirección es requerida.';
+                    }
+
+                    return errors;
+                }} */
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
                 }}
             >
                 {({
@@ -25,20 +65,37 @@ const FormOne = () => {
                     touched,
                     handleChange,
                     handleBlur,
-                    handleSubmit
+                    handleSubmit,
+                    isSubmitting,
                 }) => (
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            id="name"
-                            onChange={handleChange}
-                            value={values.name} />
-                        <button type="submit">Submit</button>
-
-                    </form>
-                )
-                }
-
+                    <Box w="50vw" p="50px">
+                        <FormControl >
+                            <form onSubmit={handleSubmit}>
+                                <Box display="flex" justifyContent="space-between">
+                                    <FormLabel >Nombre</FormLabel>
+                                    <Input w="80%" id='nombre' name='nombre' type='text' onChange={handleChange} onBlur={handleBlur} value={values.nombre} />
+                                </Box>
+                                <Text textAlign="right">{errors.nombre && touched.nombre && errors.nombre}</Text>
+                                <Box display="flex" justifyContent="space-between">
+                                    <FormLabel>Email</FormLabel>
+                                    <Input w="80%" id='email' name='email' type='text' onChange={handleChange} onBlur={handleBlur} value={values.email} />
+                                </Box>
+                                <Text textAlign="right">{errors.email && touched.email && errors.email}</Text>
+                                <Box display="flex" justifyContent="space-between">
+                                    <FormLabel>Password</FormLabel><Input w="80%" id='password' name='password' type="password" onChange={handleChange} onBlur={handleBlur} value={values.password} />
+                                    <Button pos="absolute" right={0} zIndex={10}>Ver password</Button>
+                                </Box>
+                                <Text textAlign="right">{errors.password && touched.password && errors.password}</Text>
+                                <Box display="flex" justifyContent="space-between">
+                                    <FormLabel>Direccion</FormLabel>
+                                    <Input w="80%" id='direccion' name='direccion' type='text' onChange={handleChange} onBlur={handleBlur} value={values.direccion} />
+                                </Box>
+                                <Text textAlign="right">{errors.direccion && touched.direccion && errors.direccion}</Text>
+                                <Button type='submit' disabled={isSubmitting}>Enviar</Button>
+                            </form>
+                        </FormControl>
+                    </Box>
+                )}
             </Formik>
         </div>
     )
--- src/Views/Header.jsx
+++ src/Views/Header.jsx
@@ -2,9 +2,9 @@ import { Box, Center, Heading, Link } from "@chakra-ui/react";
 
 const Header = () => {
     return (
-        <Center bg="red" w="100vw" h="10vh">
+        <Center bg="red" w="100vw" h="10vh" justifyContent="space-around">
             <Heading>logo</Heading>
-            <Box>
+            <Box display="flex" alignItems="center" gap="2rem" h="100%">
                 <Link href="/">Home</Link>
                 <Link href="/aoe">About</Link>
             </Box>
