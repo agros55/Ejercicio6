274128a198e70dfa0729f442931ed455ca7e5279
agros55 <fernandoviveros95@gmail.com>
2023-11-17 18:11:18 -0300

    Correccion error Page

--- src/Views/ErrorPage.jsx
+++ src/Views/ErrorPage.jsx
@@ -2,7 +2,7 @@ import { Center, Heading, Text } from "@chakra-ui/react";
 
 export default function ErrorPage() {
     return (
-        <Center flexDir="column" h="100vh">
+        <Center flexDir="column" h="80vh">
             <Heading>Oops!</Heading>
             <Text>Pagina no encontrada.</Text>
         </Center>
